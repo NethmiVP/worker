b80f8f996d457d5b1cba2d30130e7de836afa865
NethmiVP <nethmi.punsala@gmail.com>
2025-06-28 09:09:13 +0530

    test2

--- src/main/java/com/worker_app/controller/WorkerController.java
+++ src/main/java/com/worker_app/controller/WorkerController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//Test333
+
 @RestController
 public class WorkerController {
 
