9e8450d8cdcb51c22e6fb954fc410852fa749345
NethmiVP <nethmi.punsala@gmail.com>
2025-06-28 10:48:33 +0530

    query recorrection

--- src/main/java/com/worker_app/data/WorkerRepository.java
+++ src/main/java/com/worker_app/data/WorkerRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface WorkerRepository extends JpaRepository<Worker, Integer> {
 
-    @Query("select w from Worker w where w.fname=?1 and w.age=?2")
+    @Query("select w from Worker w where w.fname=?1 and w.location=?2")
     //defining a method
     public List<Worker> searchWorkers(String fname, String location);
 
